000287784cca09af49dc445a621536cc0aecde22
CodeTokyo <b-man225@hotmail.com>
2019-03-18 17:59:57 -0500

    Add New and Create actions for Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,23 @@ class ProductsController < ApplicationController
   # @products is what we are naming the model list. We put Product.all because Product is the model's name in the database that we are pulling from!
   @products = Product.all
   end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  def new
+    
+  end
+
+  def create
+    product = Product.new( 
+                      name: params[:name], 
+                      description: params[:description], 
+                      price: params[:price] 
+    )
+
+    product.save
+
+    redirect_to "/products"
+  end
 end
